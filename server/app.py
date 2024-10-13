8f928c3881e9d1abd4980fbea6eb59e5cf29830b
TeslaC00 <vikaskumar16122002@gmail.com>
2024-10-13 09:57:19 +0000

    added food_items API with category

--- server/app.py
+++ server/app.py
@@ -1,11 +1,17 @@
 from flask import Flask
+from database import db
+from bson import json_util
 
 app = Flask(__name__)
+collection = db["food_items"]
 
 
-@app.route("/")
-def home():
-    return "<h1>Home Route</h1>"
+@app.route("/api/food_items/<category>", methods=["GET"])
+def get_food_items_by_category(category):
+    projection = {"_id": 1, "item_name": 1, "item_category": 1, "image_url": 1}
+    documents = collection.find({"item_category": category}, projection)
+    results = json_util.dumps(documents)
+    return results
 
 
 if __name__ == "__main__":
--- server/database.py
+++ server/database.py
@@ -1,9 +1,15 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+from dotenv import load_dotenv
+import os
 
-uri: str = ""
+load_dotenv()
+
+uri: str = os.getenv("MONGO_ATLAS_URI")
 
 client = MongoClient(uri, server_api=ServerApi("1"))
+
+db = client["food-stat"]
 
 # Send a ping to confirm a successful connection
 try:
