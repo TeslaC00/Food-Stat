6a1229d5a82a361bd7adb687022783e17ce80a6f
TeslaC00 <111948419+TeslaC00@users.noreply.github.com>
2025-03-30 18:39:19 +0530

    Refactor layout, removed unused import

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import Navbar from "./components/navbar";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -29,7 +28,6 @@ export default function RootLayout({
       <body
         className={`bg-white ${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        
         {children}
       </body>
     </html>
