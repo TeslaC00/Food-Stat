1ef6a9ef2bd303fd219b115b19cd3eec87bd6003
TeslaC00 <111948419+TeslaC00@users.noreply.github.com>
2025-04-06 22:13:58 +0530

    Added food item template, update category template
    Update category template to use correct food item id

--- server/routes.py
+++ server/routes.py
@@ -81,16 +81,62 @@ def category() -> str:
     return render_template("category.jinja", food_items=food_items)
 
 
+@routes_bp.get("/food_item/<int:food_item_id>")
 def food_item(food_item_id: int) -> str:
     # TODO: add database query to get food item data
 
-    food_item = {
-        "id": 4,
-        "title": "Noodles",
-        "desc": "Long, slippery and saucy noooodles.",
-        "image_url": "https://images.unsplash.com/photo-1585032226651-759b368d7246?q=80&w=1292&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        "alt": "Noodles",
-    }
+    food_items = [
+        {
+            "id": 1,
+            "title": "Green Apple",
+            "image_url": "https://media.istockphoto.com/id/629734762/photo/green-apple-with-leaf-isolated-on-white-clipping-path-included.jpg?s=1024x1024&w=is&k=20&c=QzSTeGlb1GkZWQ0O2VBlAeB8-ZYPzXFv7YSUYxE2AoI=",
+            "alt": "Green Apple",
+            "rating": 1,
+            "energy": 1,
+            "ingredients": ["Hara", "Apple"],
+            "allergies": ["Hara ranga"],
+        },
+        {
+            "id": 2,
+            "title": "Red Apple",
+            "image_url": "https://media.istockphoto.com/id/614871876/photo/apple-isolated-on-wood-background.jpg?s=1024x1024&w=is&k=20&c=HlmdzA8HWMiVdSicwDiEa77FSxQEEvxm6nGzeSRGBZ4=",
+            "alt": "Red Apple",
+            "rating": 4.0,
+            "energy": 11,
+            "protein": 30,
+            "carbs": 50,
+            "fat": 100,
+            "ingredients": [],
+            "allergies": [],
+        },
+        {
+            "id": 3,
+            "title": "Pasta",
+            "image_url": "https://media.istockphoto.com/id/482964545/photo/arrabiata-pasta.jpg?s=1024x1024&w=is&k=20&c=WV35LbX2fkLqb2jYSPqFQcyN0OlbF_HAJ0tWbfQ9KzA=",
+            "alt": "Pasta",
+            "rating": 3.5,
+            "energy": 100,
+            "carbs": 6,
+            "fat": 22,
+            "ingredients": ["Sauce", "Namak", "Masala", "Atta"],
+            "allergies": ["Poink", "Oink", "Khujli"],
+        },
+        {
+            "id": 4,
+            "title": "Noodles",
+            "image_url": "https://images.unsplash.com/photo-1585032226651-759b368d7246?q=80&w=1292&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            "alt": "Noodles",
+            "rating": 4.5,
+            "energy": 1,
+            "protein": 23,
+            "carbs": 20,
+            "fat": 202,
+            "ingredients": ["Lamba Atta", "Chota masala", "Peela oil"],
+            "allergies": ["Nuinui", "Nainai"],
+        },
+    ]
+
+    food_item = food_items[food_item_id-1]
 
     return render_template("food_item.jinja", food_item=food_item)
 
